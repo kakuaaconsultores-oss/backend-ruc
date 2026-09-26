7d75c8c798a2ea309c1a9bd5a75b4e953b7ef174
kakuaaconsultores-oss <kakuaaconsultores@gmail.com>
2026-09-26 09:39:19 -0300

    feat: register ERP modules

--- compras_wsgi.py
+++ compras_wsgi.py
@@ -1,4 +1,7 @@
 from app import app, get_db, staff_required, usuario_required, admin_required, obtener_usuario_por_token, insertar_y_obtener_id
+from erp_modulos import register as register_erp
+register_erp(app, get_db, staff_required, usuario_required, admin_required)
+
 from compras import register
 register(app, get_db, staff_required, usuario_required, insertar_y_obtener_id)
 
